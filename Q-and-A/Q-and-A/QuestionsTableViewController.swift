fcf77a2e31fdc23b94b98b62fecd042dc82f15ea
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-25 13:57:14 -0700

    QuestionsTableViewController

---
+++ Q-and-A/Q-and-A/QuestionsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionsTableViewController.swift
+//  Q-and-A
+//
+//  Created by Hector Steven on 4/25/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell", for: indexPath)
+		cell.textLabel?.text = "\(indexPath.row)"
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(indexPath.row)
+	}
+}
